f36bc1ce83c94e8d20e4a27ea9c34ae0cf19a7fc
panleiming <panleiming@linksaas.pro>
2022-11-01 12:50:22 +0800

    adjust install doc

--- src/pages/Robot/components/UseageModal.tsx
+++ src/pages/Robot/components/UseageModal.tsx
@@ -6,22 +6,30 @@ import { writeText } from '@tauri-apps/api/clipboard';
 
 
 interface UseageModalProps {
-    projectId: string;
-    robotId: string;
-    serverAddr: string;
-    token: string;
-    onCancel: () => void;
+  projectId: string;
+  robotId: string;
+  serverAddr: string;
+  token: string;
+  onCancel: () => void;
 }
 
 export const UseageModal: React.FC<UseageModalProps> = (props) => {
-    const config =
-        `server: ${props.serverAddr}
+  const installStr = `wget https://www.linksaas.pro/release/robot
+chmod a+x ./robot
+sudo ./robot install`;
+
+  const configStr =
+    `server: ${props.serverAddr}
 metric:
-  logFile: logs/metric.log
+  logFile: /var/log/linksaas/metric.log
   # debug:-1,info:0,warn:1,error:2
   logLv: 0
-  data: data
+  data: /var/lib/linksaas/data
   scan_interval: 10
+  # 数据保存天数
+  keep_day: 30
+  # 报警最小时间间隔，单位秒
+  min_alarm_interval: 300
   cpu:
     # support param: user,system,idle,nice,iowait,irq,softirq,steal,guest,guestNice
     alarm_rule: "idle < 0.05 || system > 0.3"
@@ -30,9 +38,10 @@ metric:
       # support param: total,free,used,usedPercent,inodesTotal,inodesUsed,inodesFree,inodesUsedPercent
       alarm_rule: "usedPercent > 0.9 || inodesUsedPercent > 0.9"
   docker:
+    # name对应docker ps里面的NAMES字段
     - name: abc
       # support param: running
-      alarm_rule: "int(running) == 0"
+      alarm_rule: "running < 0.05"
   load:
     # support param: load1,load5,load15
     alarm_rule: "load1 > 20.0 || load5 > 10.0"
@@ -45,39 +54,78 @@ metric:
 agent:
   - project: ${props.projectId}
     robot: ${props.robotId}
-    token: ${props.token}        
+    token: ${props.token}
+    logFile: /var/log/linksaas/agent.log
+    # debug:-1,info:0,warn:1,error:2
+    logLv: 0        
     `;
-    return (
-        <Modal
-            title="接入说明"
-            open={true}
-            footer={null}
-            mask={false}
-            onCancel={() => props.onCancel()}>
-            <ol>
-                <li>1.下载机器人</li>
-                <li>2.安装机器人</li>
-                <li>3.使用如下配置<Button type="link" onClick={e=>{
-                  e.stopPropagation();
-                  e.preventDefault();
-                  writeText(config).then(()=>{
-                    message.info("复制成功");
-                  })
-                }}>复制</Button>
-                    <CodeEditor
-                        value={config}
-                        language="yaml"
-                        disabled
-                        style={{
-                            fontSize: 14,
-                            backgroundColor: '#f5f5f5',
-                            height: "300px",
-                            overflowY: "scroll",
-                        }}
-                    />
-                </li>
-                <li>4.启动机器人</li>
-            </ol>
-        </Modal>
-    );
+    
+  const startStr = `systemctl daemon-reload
+systemctl enable linksaas_robot
+systemctl start linksaas_robot`;
+
+  return (
+    <Modal
+      title="接入说明"
+      open={true}
+      footer={null}
+      mask={false}
+      onCancel={() => props.onCancel()}>
+      <ol>
+        <li>1. 安装机器人<Button type="link" onClick={e => {
+          e.stopPropagation();
+          e.preventDefault();
+          writeText(installStr).then(() => {
+            message.info("复制成功");
+          })
+        }}>复制</Button>
+          <CodeEditor
+            value={installStr}
+            language="bash"
+            disabled
+            style={{
+              fontSize: 14,
+              backgroundColor: '#f5f5f5',
+            }}
+          />
+        </li>
+        <li>2.修改/etc/linksaas/robot.yaml配置<Button type="link" onClick={e => {
+          e.stopPropagation();
+          e.preventDefault();
+          writeText(configStr).then(() => {
+            message.info("复制成功");
+          })
+        }}>复制</Button>
+          <CodeEditor
+            value={configStr}
+            language="yaml"
+            disabled
+            style={{
+              fontSize: 14,
+              backgroundColor: '#f5f5f5',
+              height: "200px",
+              overflowY: "scroll",
+            }}
+          />
+        </li>
+        <li>3.启动机器人<Button type="link" onClick={e => {
+          e.stopPropagation();
+          e.preventDefault();
+          writeText(startStr).then(() => {
+            message.info("复制成功");
+          })
+        }}>复制</Button>
+        <CodeEditor
+            value={startStr}
+            language="bash"
+            disabled
+            style={{
+              fontSize: 14,
+              backgroundColor: '#f5f5f5',
+            }}
+          />
+        </li>
+      </ol>
+    </Modal>
+  );
 };
