c3262c3c6570650cc229b8b891c5d93a0c5a51df
panleiming <panleiming@linksaas.pro>
2023-03-05 15:43:25 +0800

    adjust scroll bar style

--- src/components/TopNav/index.tsx
+++ src/components/TopNav/index.tsx
@@ -16,6 +16,7 @@ import { CommentOutlined, FileDoneOutlined, SettingOutlined } from '@ant-design/
 import SearchBar from '../SearchBar';
 import { LAYOUT_TYPE_CHAT, LAYOUT_TYPE_CHAT_AND_KB, LAYOUT_TYPE_KB, LAYOUT_TYPE_KB_AND_CHAT } from '@/api/project';
 import { observer } from 'mobx-react';
+import classNames from 'classnames';
 
 const SettingBtn = observer(() => {
   const appStore = useStores('appStore');
@@ -52,10 +53,11 @@ const TopNav = () => {
         <span className={activeKey == APP_PROJECT_CHAT_PATH ? s.tab_chat_active : s.tab_chat}><CommentOutlined />沟通</span>
       </Tooltip>
     } key={APP_PROJECT_CHAT_PATH} />);
+
   const kbTabPanel = (
     <Tabs.TabPane tab={
       <Tooltip title="主界面: 知识库面板" open={appStore.showProjectSetting}
-        placement={projectStore.curProject?.setting.layout_type == LAYOUT_TYPE_CHAT_AND_KB ? "right": "left"} color="orange" overlayInnerStyle={{ color: 'black' }}>
+        placement={projectStore.curProject?.setting.layout_type == LAYOUT_TYPE_CHAT_AND_KB ? "right" : "left"} color="orange" overlayInnerStyle={{ color: 'black' }}>
         <span className={activeKey == APP_PROJECT_CHAT_PATH ? s.tab_kb : s.tab_kb_active}><FileDoneOutlined />知识库</span>
       </Tooltip>
     } key={APP_PROJECT_KB_PATH} />);
@@ -72,7 +74,7 @@ const TopNav = () => {
     <div className={s.topnav}>
       <div>
         <Tabs
-          className={s.tabs}
+          className={classNames(s.tabs, [LAYOUT_TYPE_CHAT, LAYOUT_TYPE_KB].includes(projectStore.curProject?.setting.layout_type ?? LAYOUT_TYPE_CHAT_AND_KB) ? s.single : s.multi)}
           activeKey={activeKey}
           onChange={(key) => {
             if (docSpaceStore.inEdit) {
