41c57f3ab217ca396214c400ad1863ab7ac0ef48
panleiming <panleiming@linksaas.pro>
2023-03-24 21:21:17 +0800

    hide watch channel and doc in left panel when layout change

--- src/components/LeftMenu/ProjectWatch.tsx
+++ src/components/LeftMenu/ProjectWatch.tsx
@@ -8,6 +8,7 @@ import { PROJECT_CHAT_TYPE } from "@/utils/constant";
 import { LinkChannelInfo, LinkDocInfo } from "@/stores/linkAux";
 import { useHistory } from "react-router-dom";
 import { CommentOutlined, FileOutlined } from "@ant-design/icons";
+import { LAYOUT_TYPE_CHAT, LAYOUT_TYPE_CHAT_AND_KB, LAYOUT_TYPE_KB, LAYOUT_TYPE_KB_AND_CHAT } from "@/api/project";
 
 const ProjectWatch = () => {
     const history = useHistory();
@@ -19,27 +20,37 @@ const ProjectWatch = () => {
 
     return (
         <div className={s.content_wrap}>
-            {channelStore.channelList.filter(item => item.channelInfo.my_watch).map(item => (
-                <div key={item.channelInfo.channel_id} style={{ position: "relative" }}>
-                    <Badge count={item.unreadMsgCount} dot={true} style={{ position: "absolute", left: 0, top: -4 }} />
-                    <span className={classNames(s.title, (projectStore.projectChatType == PROJECT_CHAT_TYPE.PROJECT_CHAT_CHANNEL && channelStore.curChannelId == item.channelInfo.channel_id) ? s.title_active : "")}
-                        onClick={e => {
-                            e.stopPropagation();
-                            e.preventDefault();
-                            linkAuxStore.goToLink(new LinkChannelInfo("", projectStore.curProjectId, item.channelInfo.channel_id), history);
-                        }}><span className={s.collect}/><CommentOutlined />&nbsp;{item.channelInfo.basic_info.channel_name}</span>
-                </div>
-            ))}
-            {docSpaceStore.curWatchDocList.map(item => (
-                <div key={item.doc_id}>
-                    <span className={classNames(s.title, docSpaceStore.curDocId == item.doc_id ? s.title_active : "")}
-                        onClick={e => {
-                            e.stopPropagation();
-                            e.preventDefault();
-                            linkAuxStore.goToLink(new LinkDocInfo("", projectStore.curProjectId, "", item.doc_id), history);
-                        }}><span className={s.collect}/><FileOutlined />&nbsp;{item.title}</span>
-                </div>
-            ))}
+            {[LAYOUT_TYPE_CHAT_AND_KB, LAYOUT_TYPE_KB_AND_CHAT, LAYOUT_TYPE_CHAT].includes(projectStore.curProject?.setting.layout_type ?? LAYOUT_TYPE_CHAT_AND_KB) && (
+                <>
+                    {
+                        channelStore.channelList.filter(item => item.channelInfo.my_watch).map(item => (
+                            <div key={item.channelInfo.channel_id} style={{ position: "relative" }}>
+                                <Badge count={item.unreadMsgCount} dot={true} style={{ position: "absolute", left: 0, top: -4 }} />
+                                <span className={classNames(s.title, (projectStore.projectChatType == PROJECT_CHAT_TYPE.PROJECT_CHAT_CHANNEL && channelStore.curChannelId == item.channelInfo.channel_id) ? s.title_active : "")}
+                                    onClick={e => {
+                                        e.stopPropagation();
+                                        e.preventDefault();
+                                        linkAuxStore.goToLink(new LinkChannelInfo("", projectStore.curProjectId, item.channelInfo.channel_id), history);
+                                    }}><span className={s.collect} /><CommentOutlined />&nbsp;{item.channelInfo.basic_info.channel_name}</span>
+                            </div>
+                        ))
+                    }
+                </>
+            )}
+            {[LAYOUT_TYPE_CHAT_AND_KB, LAYOUT_TYPE_KB_AND_CHAT, LAYOUT_TYPE_KB].includes(projectStore.curProject?.setting.layout_type ?? LAYOUT_TYPE_CHAT_AND_KB) && (
+                <>
+                    {docSpaceStore.curWatchDocList.map(item => (
+                        <div key={item.doc_id}>
+                            <span className={classNames(s.title, docSpaceStore.curDocId == item.doc_id ? s.title_active : "")}
+                                onClick={e => {
+                                    e.stopPropagation();
+                                    e.preventDefault();
+                                    linkAuxStore.goToLink(new LinkDocInfo("", projectStore.curProjectId, "", item.doc_id), history);
+                                }}><span className={s.collect} /><FileOutlined />&nbsp;{item.title}</span>
+                        </div>
+                    ))}
+                </>
+            )}
         </div>
     )
 };
