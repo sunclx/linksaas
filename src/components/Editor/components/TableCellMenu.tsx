abf211fbd4429df71318cac9279fd4b11e7adef7
panleiming <panleiming@linksaas.pro>
2023-04-06 10:00:59 +0800

    fix table editor menu

--- src/components/Editor/components/TableCellMenu.tsx
+++ src/components/Editor/components/TableCellMenu.tsx
@@ -1,7 +1,7 @@
 import type { TableCellMenuComponentProps } from '@remirror/extension-react-tables';
 import React, { useState } from 'react';
 import { ComponentsTheme } from '@remirror/theme';
-import { Menu, Modal } from 'antd';
+import { Menu, Modal, Popover } from 'antd';
 import { useCommands } from '@remirror/react';
 import { PhotoshopPicker } from 'react-color';
 import { MenuOutlined } from '@ant-design/icons';
@@ -19,7 +19,7 @@ const TableCellMenuButton: React.FC<TableCellMenuComponentProps> = ({ setPopupOp
             }}
             style={{
                 position: 'relative',
-                right: '0px',
+                right: '10px',
                 top: '0px',
                 height: '16px',
                 width: '16px',
@@ -42,129 +42,129 @@ const TableCellMenu: React.FC<TableCellMenuComponentProps> = (props) => {
 
     return (
         <>
-            <TableCellMenuButton {...props} />
-            {props.popupOpen && (
-                <div
-                    style={{
-                        position: 'absolute',
-                        backgroundColor: 'white',
-                        width: '150px',
-                        border: '1px solid lightgray',
-                        display: 'flex',
-                        flexDirection: 'column',
-                    }}
-                    className={ComponentsTheme.MENU}
-                >
-                    <Menu mode="vertical" style={{ listStyle: "none" }} items={[
-                        {
-                            key: "rowOps",
-                            label: "表格行",
-                            children: [
-                                {
-                                    key: "addRowBefore",
-                                    label: "在上方插入一行",
-                                    onClick: () => {
-                                        commands.addTableRowBefore();
-                                        props.setPopupOpen(false);
-                                    },
+            <Popover open={props.popupOpen} placement='right' content={
+                <Menu mode="vertical" style={{ listStyle: "none" }} items={[
+                    {
+                        key: "rowOps",
+                        label: "表格行",
+                        children: [
+                            {
+                                key: "addRowBefore",
+                                label: "在上方插入一行",
+                                onClick: () => {
+                                    commands.addTableRowBefore();
+                                    props.setPopupOpen(false);
                                 },
-                                {
-                                    key: "addRowAfter",
-                                    label: "在下方插入一行",
-                                    onClick: () => {
-                                        commands.addTableRowAfter();
-                                        props.setPopupOpen(false);
-                                    },
+                            },
+                            {
+                                key: "addRowAfter",
+                                label: "在下方插入一行",
+                                onClick: () => {
+                                    commands.addTableRowAfter();
+                                    props.setPopupOpen(false);
                                 },
-                                {
-                                    key: "removeRow",
-                                    label: "删除当前行",
-                                    onClick: () => {
-                                        commands.deleteTableRow();
-                                        props.setPopupOpen(false);
-                                    },
+                            },
+                            {
+                                key: "removeRow",
+                                label: "删除当前行",
+                                danger: true,
+                                onClick: () => {
+                                    commands.deleteTableRow();
+                                    props.setPopupOpen(false);
                                 },
-                            ],
+                            },
+                        ],
+                    },
+                    {
+                        key: "colOps",
+                        label: "表格列",
+                        children: [
+                            {
+                                key: "addColBefore",
+                                label: "在前方插入一列",
+                                onClick: () => {
+                                    commands.addTableColumnBefore();
+                                    props.setPopupOpen(false);
+                                },
+                            },
+                            {
+                                key: "addColAfter",
+                                label: "在后方插入一列",
+                                onClick: () => {
+                                    commands.addTableColumnAfter();
+                                    props.setPopupOpen(false);
+                                },
+                            },
+                            {
+                                key: "removeCol",
+                                label: "删除当前列",
+                                danger: true,
+                                onClick: () => {
+                                    commands.deleteTableColumn();
+                                    props.setPopupOpen(false);
+                                },
+                            }
+                        ],
+                    },
+                    {
+                        key: "mergeSplit",
+                        label: "合并拆分",
+                        children: [
+                            {
+                                key: "merge",
+                                label: "合并单元格",
+                                disabled: !commands.mergeTableCells.enabled(),
+                                onClick: () => {
+                                    commands.mergeTableCells();
+                                    props.setPopupOpen(false);
+                                },
+                            },
+                            {
+                                key: "split",
+                                label: "拆分单元格",
+                                disabled: !commands.splitTableCell.enabled(),
+                                onClick: () => {
+                                    commands.splitTableCell();
+                                    props.setPopupOpen(false);
+                                },
+                            },
+                        ],
+                    },
+                    {
+                        key: "bgColor",
+                        label: "背景色",
+                        children: [
+                            {
+                                key: "setBgColor",
+                                label: "设置背景色",
+                                onClick: () => {
+                                    props.setPopupOpen(false);
+                                    setShowColorModal(true);
+                                },
+                            },
+                            {
+                                key: "cancelBgColor",
+                                label: "清除背景色",
+                                onClick: () => {
+                                    commands.setTableCellBackground(null);
+                                    props.setPopupOpen(false);
+                                },
+                            },
+                        ],
+                    },
+                    {
+                        key: "deleteTable",
+                        label: "删除表格",
+                        danger: true,
+                        onClick: () => {
+                            commands.deleteTable();
+                            props.setPopupOpen(false);
                         },
-                        {
-                            key: "colOps",
-                            label: "表格列",
-                            children: [
-                                {
-                                    key: "addColBefore",
-                                    label: "在前方插入一列",
-                                    onClick: () => {
-                                        commands.addTableColumnBefore();
-                                        props.setPopupOpen(false);
-                                    },
-                                },
-                                {
-                                    key: "addColAfter",
-                                    label: "在后方插入一列",
-                                    onClick: () => {
-                                        commands.addTableColumnAfter();
-                                        props.setPopupOpen(false);
-                                    },
-                                },
-                                {
-                                    key: "removeCol",
-                                    label: "删除当前列",
-                                    onClick: () => {
-                                        commands.deleteTableColumn();
-                                        props.setPopupOpen(false);
-                                    },
-                                }
-                            ],
-                        },
-                        {
-                            key: "mergeSplit",
-                            label: "合并拆分",
-                            children: [
-                                {
-                                    key: "merge",
-                                    label: "合并单元格",
-                                    disabled: !commands.mergeTableCells.enabled(),
-                                    onClick: () => {
-                                        commands.mergeTableCells();
-                                        props.setPopupOpen(false);
-                                    },
-                                },
-                                {
-                                    key: "split",
-                                    label: "拆分单元格",
-                                    disabled: !commands.splitTableCell.enabled(),
-                                    onClick: () => {
-                                        commands.splitTableCell();
-                                        props.setPopupOpen(false);
-                                    },
-                                },
-                            ],
-                        },
-                        {
-                            key: "bgColor",
-                            label: "背景色",
-                            children: [
-                                {
-                                    key: "setBgColor",
-                                    label: "设置背景色",
-                                    onClick: () => {
-                                        props.setPopupOpen(false);
-                                        setShowColorModal(true);
-                                    },
-                                },
-                                {
-                                    key: "cancelBgColor",
-                                    label: "清除背景色",
-                                    onClick: () => {
-                                        commands.setTableCellBackground(null);
-                                        props.setPopupOpen(false);
-                                    },
-                                },
-                            ],
-                        },
-                    ]} />
-                </div>
-            )}
+                    }
+                ]} />
+            }>
+                <TableCellMenuButton {...props} />
+            </Popover>
             {showColorModal == true && (
                 <Modal closable={false} open footer={null} bodyStyle={{ padding: "0px 0px" }} width={500}>
                     <PhotoshopPicker header='选择背景色'
