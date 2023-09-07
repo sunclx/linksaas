353ad63bec8cae920e2ee5aa07e1603e6cd04053
panleiming <panleiming@linksaas.pro>
2023-09-07 14:17:16 +0800

    add simple mode in crate project

--- src/components/LeftMenu/CreatedProject.tsx
+++ src/components/LeftMenu/CreatedProject.tsx
@@ -1,10 +1,10 @@
 import ActionModal from '@/components/ActionModal';
-import { Form, Input, message } from 'antd';
+import { Form, Input, Radio, message } from 'antd';
 import type { FC } from 'react';
 import React from 'react';
 import Button from '../Button';
 import type { BasicProjectInfo } from '@/api/project';
-import { create } from '@/api/project';
+import { create, update_setting } from '@/api/project';
 import { useStores } from '@/hooks';
 import { request } from '@/utils/request';
 import { useSimpleEditor } from '@/components/Editor';
@@ -22,12 +22,14 @@ const CreatedProject: FC<CreatedProjectProps> = (props) => {
   const history = useHistory();
 
   const [form] = Form.useForm();
+  form.setFieldsValue({ project_name: "", simple_mode: true })
+
   const userStore = useStores('userStore');
   const projectStore = useStores('projectStore');
 
   const { editor, editorRef } = useSimpleEditor("请输入项目描述");
 
-  const submit = async (values: { project_name: string }) => {
+  const submit = async (values: { project_name: string, simple_mode: boolean }) => {
     const content = editorRef.current?.getContent() ?? { type: "doc" };
 
     const data: BasicProjectInfo = {
@@ -37,6 +39,43 @@ const CreatedProject: FC<CreatedProjectProps> = (props) => {
     try {
       const res = await request(create(userStore.sessionId, data));
       message.success('创建项目成功');
+      if (values.simple_mode) {
+        await request(update_setting({
+          session_id: userStore.sessionId,
+          project_id: res.project_id,
+          setting: {
+            disable_member_appraise: true,
+            disable_test_case: true,
+            disable_server_agent: true,
+            disable_ext_event: true,
+            disable_app_store: true,
+            disable_data_anno: true,
+            disable_api_collection: true,
+            disable_code_comment: true,
+
+            disable_chat: true,
+            disable_kb: true,
+            disable_work_plan: false,
+
+            min_pure_text_len_in_chat: 0,
+            disable_widget_in_chat: true,
+
+            layout_type: 0, //后续删除字段
+            disable_sprit: true,//后续删除字段
+
+            hide_custom_event: true,
+            hide_custom_event_for_admin: false,
+
+            hide_project_info: true,
+            hide_bulletin: true,
+            hide_user_goal: true,
+            hide_user_award: true,
+            hide_my_todo_task: true,
+            hide_my_todo_bug: true,
+            hide_extra_info: true,
+          },
+        }));
+      }
       await projectStore.updateProject(res.project_id);
       onChange(false);
       projectStore.setCurProjectId(res.project_id).then(() => {
@@ -45,6 +84,7 @@ const CreatedProject: FC<CreatedProjectProps> = (props) => {
       });
     } catch (error) { }
   };
+
 
   return (
     <ActionModal open={visible} title="创建项目" width={800} onCancel={() => onChange(false)}>
@@ -55,6 +95,14 @@ const CreatedProject: FC<CreatedProjectProps> = (props) => {
           rules={[{ required: true, message: '项目名称必填' }]}
         >
           <Input allowClear placeholder={`请输入项目名称`} style={{ borderRadius: '6px' }} />
+        </Form.Item>
+        <Form.Item label="模式" name="simple_mode"
+          rules={[{ required: true }]}
+        >
+          <Radio.Group>
+            <Radio value={true}>简单模式</Radio>
+            <Radio value={false}>专家模式</Radio>
+          </Radio.Group>
         </Form.Item>
         <Form.Item name="project_desc" label="项目介绍">
           <div className="_projectEditContext" style={{ marginTop: '-12px' }}>
