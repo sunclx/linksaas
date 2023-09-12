10011ddca7195adbb3700a467ec73b2090f11c97
panleiming <panleiming@linksaas.pro>
2023-09-12 13:54:09 +0800

    add http custom api

---
+++ src-tauri/src/http_custom_api_plugin.rs
@@ -0,0 +1,327 @@
+use crate::notice_decode::new_wrong_session_notice;
+use proto_gen_rust::http_custom_api::http_custom_api_client::HttpCustomApiClient;
+use proto_gen_rust::http_custom_api::*;
+use tauri::{
+    plugin::{Plugin, Result as PluginResult},
+    AppHandle, Invoke, PageLoadPayload, Runtime, Window,
+};
+
+#[tauri::command]
+async fn create_custom<R: Runtime>(
+    app_handle: AppHandle<R>,
+    window: Window<R>,
+    request: CreateCustomRequest,
+) -> Result<CreateCustomResponse, String> {
+    let chan = super::get_grpc_chan(&app_handle).await;
+    if (&chan).is_none() {
+        return Err("no grpc conn".into());
+    }
+    let mut client = HttpCustomApiClient::new(chan.unwrap());
+    match client.create_custom(request).await {
+        Ok(response) => {
+            let inner_resp = response.into_inner();
+            if inner_resp.code == create_custom_response::Code::WrongSession as i32 {
+                if let Err(err) = window.emit("notice", new_wrong_session_notice("create_custom".into())) {
+                    println!("{:?}", err);
+                }
+            }
+            return Ok(inner_resp);
+        }
+        Err(status) => Err(status.message().into()),
+    }
+}
+
+#[tauri::command]
+async fn get_custom<R: Runtime>(
+    app_handle: AppHandle<R>,
+    window: Window<R>,
+    request: GetCustomRequest,
+) -> Result<GetCustomResponse, String> {
+    let chan = super::get_grpc_chan(&app_handle).await;
+    if (&chan).is_none() {
+        return Err("no grpc conn".into());
+    }
+    let mut client = HttpCustomApiClient::new(chan.unwrap());
+    match client.get_custom(request).await {
+        Ok(response) => {
+            let inner_resp = response.into_inner();
+            if inner_resp.code == get_custom_response::Code::WrongSession as i32 {
+                if let Err(err) = window.emit("notice", new_wrong_session_notice("get_custom".into())) {
+                    println!("{:?}", err);
+                }
+            }
+            return Ok(inner_resp);
+        }
+        Err(status) => Err(status.message().into()),
+    }
+}
+
+#[tauri::command]
+async fn update_custom<R: Runtime>(
+    app_handle: AppHandle<R>,
+    window: Window<R>,
+    request: UpdateCustomRequest,
+) -> Result<UpdateCustomResponse, String> {
+    let chan = super::get_grpc_chan(&app_handle).await;
+    if (&chan).is_none() {
+        return Err("no grpc conn".into());
+    }
+    let mut client = HttpCustomApiClient::new(chan.unwrap());
+    match client.update_custom(request).await {
+        Ok(response) => {
+            let inner_resp = response.into_inner();
+            if inner_resp.code == update_custom_response::Code::WrongSession as i32 {
+                if let Err(err) = window.emit("notice", new_wrong_session_notice("update_custom".into())) {
+                    println!("{:?}", err);
+                }
+            }
+            return Ok(inner_resp);
+        }
+        Err(status) => Err(status.message().into()),
+    }
+}
+
+#[tauri::command]
+async fn create_group<R: Runtime>(
+    app_handle: AppHandle<R>,
+    window: Window<R>,
+    request: CreateGroupRequest,
+) -> Result<CreateGroupResponse, String> {
+    let chan = super::get_grpc_chan(&app_handle).await;
+    if (&chan).is_none() {
+        return Err("no grpc conn".into());
+    }
+    let mut client = HttpCustomApiClient::new(chan.unwrap());
+    match client.create_group(request).await {
+        Ok(response) => {
+            let inner_resp = response.into_inner();
+            if inner_resp.code == create_group_response::Code::WrongSession as i32 {
+                if let Err(err) = window.emit("notice", new_wrong_session_notice("create_group".into())) {
+                    println!("{:?}", err);
+                }
+            }
+            return Ok(inner_resp);
+        }
+        Err(status) => Err(status.message().into()),
+    }
+}
+
+#[tauri::command]
+async fn update_group<R: Runtime>(
+    app_handle: AppHandle<R>,
+    window: Window<R>,
+    request: UpdateGroupRequest,
+) -> Result<UpdateGroupResponse, String> {
+    let chan = super::get_grpc_chan(&app_handle).await;
+    if (&chan).is_none() {
+        return Err("no grpc conn".into());
+    }
+    let mut client = HttpCustomApiClient::new(chan.unwrap());
+    match client.update_group(request).await {
+        Ok(response) => {
+            let inner_resp = response.into_inner();
+            if inner_resp.code == update_group_response::Code::WrongSession as i32 {
+                if let Err(err) = window.emit("notice", new_wrong_session_notice("update_group".into())) {
+                    println!("{:?}", err);
+                }
+            }
+            return Ok(inner_resp);
+        }
+        Err(status) => Err(status.message().into()),
+    }
+}
+
+#[tauri::command]
+async fn list_group<R: Runtime>(
+    app_handle: AppHandle<R>,
+    window: Window<R>,
+    request: ListGroupRequest,
+) -> Result<ListGroupResponse, String> {
+    let chan = super::get_grpc_chan(&app_handle).await;
+    if (&chan).is_none() {
+        return Err("no grpc conn".into());
+    }
+    let mut client = HttpCustomApiClient::new(chan.unwrap());
+    match client.list_group(request).await {
+        Ok(response) => {
+            let inner_resp = response.into_inner();
+            if inner_resp.code == list_group_response::Code::WrongSession as i32 {
+                if let Err(err) = window.emit("notice", new_wrong_session_notice("list_group".into())) {
+                    println!("{:?}", err);
+                }
+            }
+            return Ok(inner_resp);
+        }
+        Err(status) => Err(status.message().into()),
+    }
+}
+
+#[tauri::command]
+async fn remove_group<R: Runtime>(
+    app_handle: AppHandle<R>,
+    window: Window<R>,
+    request: RemoveGroupRequest,
+) -> Result<RemoveGroupResponse, String> {
+    let chan = super::get_grpc_chan(&app_handle).await;
+    if (&chan).is_none() {
+        return Err("no grpc conn".into());
+    }
+    let mut client = HttpCustomApiClient::new(chan.unwrap());
+    match client.remove_group(request).await {
+        Ok(response) => {
+            let inner_resp = response.into_inner();
+            if inner_resp.code == remove_group_response::Code::WrongSession as i32 {
+                if let Err(err) = window.emit("notice", new_wrong_session_notice("remove_group".into())) {
+                    println!("{:?}", err);
+                }
+            }
+            return Ok(inner_resp);
+        }
+        Err(status) => Err(status.message().into()),
+    }
+}
+
+#[tauri::command]
+async fn create_api_item<R: Runtime>(
+    app_handle: AppHandle<R>,
+    window: Window<R>,
+    request: CreateApiItemRequest,
+) -> Result<CreateApiItemResponse, String> {
+    let chan = super::get_grpc_chan(&app_handle).await;
+    if (&chan).is_none() {
+        return Err("no grpc conn".into());
+    }
+    let mut client = HttpCustomApiClient::new(chan.unwrap());
+    match client.create_api_item(request).await {
+        Ok(response) => {
+            let inner_resp = response.into_inner();
+            if inner_resp.code == create_api_item_response::Code::WrongSession as i32 {
+                if let Err(err) = window.emit("notice", new_wrong_session_notice("create_api_item".into())) {
+                    println!("{:?}", err);
+                }
+            }
+            return Ok(inner_resp);
+        }
+        Err(status) => Err(status.message().into()),
+    }
+}
+
+#[tauri::command]
+async fn update_api_item<R: Runtime>(
+    app_handle: AppHandle<R>,
+    window: Window<R>,
+    request: UpdateApiItemRequest,
+) -> Result<UpdateApiItemResponse, String> {
+    let chan = super::get_grpc_chan(&app_handle).await;
+    if (&chan).is_none() {
+        return Err("no grpc conn".into());
+    }
+    let mut client = HttpCustomApiClient::new(chan.unwrap());
+    match client.update_api_item(request).await {
+        Ok(response) => {
+            let inner_resp = response.into_inner();
+            if inner_resp.code == update_api_item_response::Code::WrongSession as i32 {
+                if let Err(err) = window.emit("notice", new_wrong_session_notice("update_api_item".into())) {
+                    println!("{:?}", err);
+                }
+            }
+            return Ok(inner_resp);
+        }
+        Err(status) => Err(status.message().into()),
+    }
+}
+
+#[tauri::command]
+async fn list_api_item<R: Runtime>(
+    app_handle: AppHandle<R>,
+    window: Window<R>,
+    request: ListApiItemRequest,
+) -> Result<ListApiItemResponse, String> {
+    let chan = super::get_grpc_chan(&app_handle).await;
+    if (&chan).is_none() {
+        return Err("no grpc conn".into());
+    }
+    let mut client = HttpCustomApiClient::new(chan.unwrap());
+    match client.list_api_item(request).await {
+        Ok(response) => {
+            let inner_resp = response.into_inner();
+            if inner_resp.code == list_api_item_response::Code::WrongSession as i32 {
+                if let Err(err) = window.emit("notice", new_wrong_session_notice("list_api_item".into())) {
+                    println!("{:?}", err);
+                }
+            }
+            return Ok(inner_resp);
+        }
+        Err(status) => Err(status.message().into()),
+    }
+}
+
+#[tauri::command]
+async fn remove_api_item<R: Runtime>(
+    app_handle: AppHandle<R>,
+    window: Window<R>,
+    request: RemoveApiItemRequest,
+) -> Result<RemoveApiItemResponse, String> {
+    let chan = super::get_grpc_chan(&app_handle).await;
+    if (&chan).is_none() {
+        return Err("no grpc conn".into());
+    }
+    let mut client = HttpCustomApiClient::new(chan.unwrap());
+    match client.remove_api_item(request).await {
+        Ok(response) => {
+            let inner_resp = response.into_inner();
+            if inner_resp.code == remove_api_item_response::Code::WrongSession as i32 {
+                if let Err(err) = window.emit("notice", new_wrong_session_notice("remove_api_item".into())) {
+                    println!("{:?}", err);
+                }
+            }
+            return Ok(inner_resp);
+        }
+        Err(status) => Err(status.message().into()),
+    }
+}
+
+pub struct HttpCustomApiPlugin<R: Runtime> {
+    invoke_handler: Box<dyn Fn(Invoke<R>) + Send + Sync + 'static>,
+}
+
+impl<R: Runtime> HttpCustomApiPlugin<R> {
+    pub fn new() -> Self {
+        Self {
+            invoke_handler: Box::new(tauri::generate_handler![
+                create_custom,
+                get_custom,
+                update_custom,
+                create_group,
+                update_group,
+                list_group,
+                remove_group,
+                create_api_item,
+                update_api_item,
+                list_api_item,
+                remove_api_item,
+            ]),
+        }
+    }
+}
+
+impl<R: Runtime> Plugin<R> for HttpCustomApiPlugin<R> {
+    fn name(&self) -> &'static str {
+        "http_custom_api"
+    }
+    fn initialization_script(&self) -> Option<String> {
+        None
+    }
+
+    fn initialize(&mut self, _app: &AppHandle<R>, _config: serde_json::Value) -> PluginResult<()> {
+        Ok(())
+    }
+
+    fn created(&mut self, _window: Window<R>) {}
+
+    fn on_page_load(&mut self, _window: Window<R>, _payload: PageLoadPayload) {}
+
+    fn extend_api(&mut self, message: Invoke<R>) {
+        (self.invoke_handler)(message)
+    }
+}
---
+++ src/api/http_custom.ts
@@ -0,0 +1,335 @@
+import { invoke } from '@tauri-apps/api/tauri';
+
+export type HTTP_BODY_TYPE = number;
+
+export const HTTP_BODY_NONE: HTTP_BODY_TYPE = 0;
+export const HTTP_BODY_TEXT: HTTP_BODY_TYPE = 1;
+export const HTTP_BODY_URL_ENCODE: HTTP_BODY_TYPE = 2;
+export const HTTP_BODY_MULTI_PART: HTTP_BODY_TYPE = 3;
+
+export type CustomExtraInfo = {
+    api_coll_id: string;
+    net_protocol: string;
+}
+
+export type ApiGroupInfo = {
+    group_id: string;
+    group_name: string;
+    item_count: number;
+};
+
+export type HttpKeyValue = {
+    key: string;
+    value: string;
+};
+
+export type UrlEncodeBody = {
+    param_list: HttpKeyValue[];
+};
+
+export type MultiPart = {
+    key: string;
+    value: string;
+    is_file: boolean;
+};
+
+export type MultiPartBody = {
+    part_list: MultiPart[];
+};
+
+export type Body = {
+    NodyBody?: string;
+    TextBody?: string;
+    UrlEncodeBody?: UrlEncodeBody;
+    MultiPartBody?: MultiPartBody;
+};
+
+export type ApiItemInfo = {
+    api_item_id: string;
+    api_item_name: string;
+    group_id: string;
+    method: string;
+    url: string;
+    param_list: HttpKeyValue[];
+    header_list: HttpKeyValue[];
+    body_type: HTTP_BODY_TYPE;
+    body: Body;
+};
+
+
+export type CreateCustomRequest = {
+    session_id: string;
+    project_id: string;
+    name: string;
+    default_addr: string;
+    net_protocol: string;
+};
+
+export type CreateCustomResponse = {
+    code: number;
+    err_msg: string;
+    api_coll_id: string;
+};
+
+
+export type GetCustomRequest = {
+    session_id: string;
+    project_id: string;
+    api_coll_id: string;
+};
+
+export type GetCustomResponse = {
+    code: number;
+    err_msg: string;
+    extra_info: CustomExtraInfo;
+};
+
+
+export type UpdateCustomRequest = {
+    session_id: string;
+    project_id: string;
+    api_coll_id: string;
+    net_protocol: string;
+};
+
+export type UpdateCustomResponse = {
+    code: number;
+    err_msg: string;
+};
+
+
+export type CreateGroupRequest = {
+    session_id: string;
+    project_id: string;
+    api_coll_id: string;
+    group_name: string;
+};
+
+export type CreateGroupResponse = {
+    code: number;
+    err_msg: string;
+    group_id: string;
+};
+
+
+export type UpdateGroupRequest = {
+    session_id: string;
+    project_id: string;
+    api_coll_id: string;
+    group_id: string;
+    group_name: string;
+};
+
+export type UpdateGroupResponse = {
+    code: number;
+    err_msg: string;
+};
+
+
+export type ListGroupRequest = {
+    session_id: string;
+    project_id: string;
+    api_coll_id: string;
+};
+
+export type ListGroupResponse = {
+    code: number;
+    err_msg: string;
+    group_list: ApiGroupInfo[];
+};
+
+
+export type RemoveGroupRequest = {
+    session_id: string;
+    project_id: string;
+    api_coll_id: string;
+    group_id: string;
+};
+
+export type RemoveGroupResponse = {
+    code: number;
+    err_msg: string;
+};
+
+
+export type CreateApiItemRequest = {
+    session_id: string;
+    project_id: string;
+    api_coll_id: string;
+    group_id: string;
+    api_item_name: string;
+    method: string;
+    url: string;
+    param_list: HttpKeyValue[];
+    header_list: HttpKeyValue[];
+    body_type: HTTP_BODY_TYPE;
+    body: Body;
+};
+
+
+export type CreateApiItemResponse = {
+    code: number;
+    err_msg: string;
+    api_item_id: string;
+};
+
+
+export type UpdateApiItemRequest = {
+    session_id: string;
+    project_id: string;
+    api_coll_id: string;
+    group_id: string;
+    api_item_id: string;
+    api_item_name: string;
+    method: string;
+    url: string;
+    param_list: HttpKeyValue[];
+    header_list: HttpKeyValue[];
+    body_type: HTTP_BODY_TYPE;
+    body: Body;
+};
+
+
+export type UpdateApiItemResponse = {
+    code: number;
+    err_msg: string;
+};
+
+
+export type ListApiItemRequest = {
+    session_id: string;
+    project_id: string;
+    api_coll_id: string;
+    filter_by_group_id: boolean;
+    group_id: string;
+};
+
+export type ListApiItemResponse = {
+    code: number;
+    err_msg: string;
+    item_list: ApiItemInfo[];
+};
+
+
+export type RemoveApiItemRequest = {
+    session_id: string;
+    project_id: string;
+    api_coll_id: string;
+    api_item_id: string;
+};
+
+export type RemoveApiItemResponse = {
+    code: number;
+    err_msg: string;
+};
+
+//创建自定义API
+export async function create_custom(request: CreateCustomRequest): Promise<CreateCustomResponse> {
+    const cmd = 'plugin:http_custom_api|create_custom';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<CreateCustomResponse>(cmd, {
+        request,
+    });
+}
+
+//获取自定义API
+export async function get_custom(request: GetCustomRequest): Promise<GetCustomResponse> {
+    const cmd = 'plugin:http_custom_api|get_custom';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<GetCustomResponse>(cmd, {
+        request,
+    });
+}
+
+//更新自定义API
+export async function update_custom(request: UpdateCustomRequest): Promise<UpdateCustomResponse> {
+    const cmd = 'plugin:http_custom_api|update_custom';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<UpdateCustomResponse>(cmd, {
+        request,
+    });
+}
+
+//创建分组
+export async function create_group(request: CreateGroupRequest): Promise<CreateGroupResponse> {
+    const cmd = 'plugin:http_custom_api|create_group';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<CreateGroupResponse>(cmd, {
+        request,
+    });
+}
+
+//更新分组
+export async function update_group(request: UpdateGroupRequest): Promise<UpdateGroupResponse> {
+    const cmd = 'plugin:http_custom_api|update_group';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<UpdateGroupResponse>(cmd, {
+        request,
+    });
+}
+
+//列出分组
+export async function list_group(request: ListGroupRequest): Promise<ListGroupResponse> {
+    const cmd = 'plugin:http_custom_api|list_group';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<ListGroupResponse>(cmd, {
+        request,
+    });
+}
+
+//删除分组
+export async function remove_group(request: RemoveGroupRequest): Promise<RemoveGroupResponse> {
+    const cmd = 'plugin:http_custom_api|remove_group';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<RemoveGroupResponse>(cmd, {
+        request,
+    });
+}
+
+//创建接口
+export async function create_api_item(request: CreateApiItemRequest): Promise<CreateApiItemResponse> {
+    const cmd = 'plugin:http_custom_api|create_api_item';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<CreateApiItemResponse>(cmd, {
+        request,
+    });
+}
+
+//更新接口
+export async function update_api_item(request: UpdateApiItemRequest): Promise<UpdateApiItemResponse> {
+    const cmd = 'plugin:http_custom_api|update_api_item';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<UpdateApiItemResponse>(cmd, {
+        request,
+    });
+}
+
+//列出接口
+export async function list_api_item(request: ListApiItemRequest): Promise<ListApiItemResponse> {
+    const cmd = 'plugin:http_custom_api|list_api_item';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<ListApiItemResponse>(cmd, {
+        request,
+    });
+}
+
+//删除接口
+export async function remove_api_item(request: RemoveApiItemRequest): Promise<RemoveApiItemResponse> {
+    const cmd = 'plugin:http_custom_api|remove_api_item';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<RemoveApiItemResponse>(cmd, {
+        request,
+    });
+}
+
