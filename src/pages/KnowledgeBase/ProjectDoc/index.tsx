b178052cc973e8a3b77ef16a3d33afc53c0db3cf
panleiming <panleiming@linksaas.pro>
2023-11-01 12:48:23 +0800

    update version and add change log

--- src/pages/KnowledgeBase/ProjectDoc/index.tsx
+++ src/pages/KnowledgeBase/ProjectDoc/index.tsx
@@ -24,7 +24,7 @@ const ProjectDoc = () => {
           const onLeave = docStore.onLeave;
           docStore.clearCheckLeave();
           docStore.reset();
-          if (onLeave != undefined) {
+          if (onLeave != null) {
             onLeave();
           }
         }}
