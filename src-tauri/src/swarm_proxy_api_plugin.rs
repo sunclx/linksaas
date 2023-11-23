b02dd7ef1f69f1d41fdb86efe71e1981ba847152
panleiming <panleiming@linksaas.pro>
2023-11-23 11:09:00 +0800

    add swarm api

--- src-tauri/src/swarm_proxy_api_plugin.rs
+++ src-tauri/src/swarm_proxy_api_plugin.rs
@@ -1,5 +1,5 @@
-use proto_gen_rust::k8s_proxy_api::k8s_proxy_api_client::K8sProxyApiClient;
-use proto_gen_rust::k8s_proxy_api::*;
+use proto_gen_rust::swarm_proxy_api::swarm_proxy_api_client::SwarmProxyApiClient;
+use proto_gen_rust::swarm_proxy_api::*;
 use std::time::Duration;
 use tauri::{
     plugin::{Plugin, Result as PluginResult},
@@ -39,16 +39,16 @@ async fn conn_server(addr: String) -> Result<Channel, String> {
 }
 
 #[tauri::command]
-async fn get_resource(
+async fn list_name_space(
     serv_addr: String,
-    request: GetResourceRequest,
-) -> Result<GetResourceResponse, String> {
+    request: ListNameSpaceRequest,
+) -> Result<ListNameSpaceResponse, String> {
     let chan = conn_server(serv_addr).await;
     if chan.is_err() {
         return Err(chan.err().unwrap());
     }
-    let mut client = K8sProxyApiClient::new(chan.unwrap());
-    match client.get_resource(request).await {
+    let mut client = SwarmProxyApiClient::new(chan.unwrap());
+    match client.list_name_space(request).await {
         Ok(response) => {
             return Ok(response.into_inner());
         }
@@ -57,16 +57,34 @@ async fn get_resource(
 }
 
 #[tauri::command]
-async fn list_resource(
+async fn list_service(
     serv_addr: String,
-    request: ListResourceRequest,
-) -> Result<ListResourceResponse, String> {
+    request: ListServiceRequest,
+) -> Result<ListServiceResponse, String> {
     let chan = conn_server(serv_addr).await;
     if chan.is_err() {
         return Err(chan.err().unwrap());
     }
-    let mut client = K8sProxyApiClient::new(chan.unwrap());
-    match client.list_resource(request).await {
+    let mut client = SwarmProxyApiClient::new(chan.unwrap());
+    match client.list_service(request).await {
+        Ok(response) => {
+            return Ok(response.into_inner());
+        }
+        Err(status) => Err(status.message().into()),
+    }
+}
+
+#[tauri::command]
+async fn list_task(
+    serv_addr: String,
+    request: ListTaskRequest,
+) -> Result<ListTaskResponse, String> {
+    let chan = conn_server(serv_addr).await;
+    if chan.is_err() {
+        return Err(chan.err().unwrap());
+    }
+    let mut client = SwarmProxyApiClient::new(chan.unwrap());
+    match client.list_task(request).await {
         Ok(response) => {
             return Ok(response.into_inner());
         }
@@ -83,7 +101,7 @@ async fn update_image(
     if chan.is_err() {
         return Err(chan.err().unwrap());
     }
-    let mut client = K8sProxyApiClient::new(chan.unwrap());
+    let mut client = SwarmProxyApiClient::new(chan.unwrap());
     match client.update_image(request).await {
         Ok(response) => {
             return Ok(response.into_inner());
@@ -101,7 +119,7 @@ async fn update_scale(
     if chan.is_err() {
         return Err(chan.err().unwrap());
     }
-    let mut client = K8sProxyApiClient::new(chan.unwrap());
+    let mut client = SwarmProxyApiClient::new(chan.unwrap());
     match client.update_scale(request).await {
         Ok(response) => {
             return Ok(response.into_inner());
@@ -111,16 +129,16 @@ async fn update_scale(
 }
 
 #[tauri::command]
-async fn list_resource_perm(
+async fn get_name_space_perm(
     serv_addr: String,
-    request: ListResourcePermRequest,
-) -> Result<ListResourcePermResponse, String> {
+    request: GetNameSpacePermRequest,
+) -> Result<GetNameSpacePermResponse, String> {
     let chan = conn_server(serv_addr).await;
     if chan.is_err() {
         return Err(chan.err().unwrap());
     }
-    let mut client = K8sProxyApiClient::new(chan.unwrap());
-    match client.list_resource_perm(request).await {
+    let mut client = SwarmProxyApiClient::new(chan.unwrap());
+    match client.get_name_space_perm(request).await {
         Ok(response) => {
             return Ok(response.into_inner());
         }
@@ -129,16 +147,16 @@ async fn list_resource_perm(
 }
 
 #[tauri::command]
-async fn set_resource_perm(
+async fn set_name_space_perm(
     serv_addr: String,
-    request: SetResourcePermRequest,
-) -> Result<SetResourcePermResponse, String> {
+    request: SetNameSpacePermRequest,
+) -> Result<SetNameSpacePermResponse, String> {
     let chan = conn_server(serv_addr).await;
     if chan.is_err() {
         return Err(chan.err().unwrap());
     }
-    let mut client = K8sProxyApiClient::new(chan.unwrap());
-    match client.set_resource_perm(request).await {
+    let mut client = SwarmProxyApiClient::new(chan.unwrap());
+    match client.set_name_space_perm(request).await {
         Ok(response) => {
             return Ok(response.into_inner());
         }
@@ -147,12 +165,15 @@ async fn set_resource_perm(
 }
 
 #[tauri::command]
-async fn open_log(serv_addr: String, request: OpenLogRequest) -> Result<OpenLogResponse, String> {
+async fn open_log(
+    serv_addr: String,
+    request: OpenLogRequest,
+) -> Result<OpenLogResponse, String> {
     let chan = conn_server(serv_addr).await;
     if chan.is_err() {
         return Err(chan.err().unwrap());
     }
-    let mut client = K8sProxyApiClient::new(chan.unwrap());
+    let mut client = SwarmProxyApiClient::new(chan.unwrap());
     match client.open_log(request).await {
         Ok(response) => {
             return Ok(response.into_inner());
@@ -162,12 +183,15 @@ async fn open_log(serv_addr: String, request: OpenLogRequest) -> Result<OpenLogR
 }
 
 #[tauri::command]
-async fn read_log(serv_addr: String, request: ReadLogRequest) -> Result<ReadLogResponse, String> {
+async fn read_log(
+    serv_addr: String,
+    request: ReadLogRequest,
+) -> Result<ReadLogResponse, String> {
     let chan = conn_server(serv_addr).await;
     if chan.is_err() {
         return Err(chan.err().unwrap());
     }
-    let mut client = K8sProxyApiClient::new(chan.unwrap());
+    let mut client = SwarmProxyApiClient::new(chan.unwrap());
     match client.read_log(request).await {
         Ok(response) => {
             return Ok(response.into_inner());
@@ -185,7 +209,7 @@ async fn open_term(
     if chan.is_err() {
         return Err(chan.err().unwrap());
     }
-    let mut client = K8sProxyApiClient::new(chan.unwrap());
+    let mut client = SwarmProxyApiClient::new(chan.unwrap());
     match client.open_term(request).await {
         Ok(response) => {
             return Ok(response.into_inner());
@@ -203,7 +227,7 @@ async fn write_term(
     if chan.is_err() {
         return Err(chan.err().unwrap());
     }
-    let mut client = K8sProxyApiClient::new(chan.unwrap());
+    let mut client = SwarmProxyApiClient::new(chan.unwrap());
     match client.write_term(request).await {
         Ok(response) => {
             return Ok(response.into_inner());
@@ -221,7 +245,7 @@ async fn read_term(
     if chan.is_err() {
         return Err(chan.err().unwrap());
     }
-    let mut client = K8sProxyApiClient::new(chan.unwrap());
+    let mut client = SwarmProxyApiClient::new(chan.unwrap());
     match client.read_term(request).await {
         Ok(response) => {
             return Ok(response.into_inner());
@@ -239,7 +263,7 @@ async fn set_term_size(
     if chan.is_err() {
         return Err(chan.err().unwrap());
     }
-    let mut client = K8sProxyApiClient::new(chan.unwrap());
+    let mut client = SwarmProxyApiClient::new(chan.unwrap());
     match client.set_term_size(request).await {
         Ok(response) => {
             return Ok(response.into_inner());
@@ -248,20 +272,21 @@ async fn set_term_size(
     }
 }
 
-pub struct K8sProxyApiPlugin<R: Runtime> {
+pub struct SwarmProxyApiPlugin<R: Runtime> {
     invoke_handler: Box<dyn Fn(Invoke<R>) + Send + Sync + 'static>,
 }
 
-impl<R: Runtime> K8sProxyApiPlugin<R> {
+impl<R: Runtime> SwarmProxyApiPlugin<R> {
     pub fn new() -> Self {
         Self {
             invoke_handler: Box::new(tauri::generate_handler![
-                get_resource,
-                list_resource,
+                list_name_space,
+                list_service,
+                list_task,
                 update_image,
                 update_scale,
-                list_resource_perm,
-                set_resource_perm,
+                get_name_space_perm,
+                set_name_space_perm,
                 open_log,
                 read_log,
                 open_term,
@@ -273,9 +298,9 @@ impl<R: Runtime> K8sProxyApiPlugin<R> {
     }
 }
 
-impl<R: Runtime> Plugin<R> for K8sProxyApiPlugin<R> {
+impl<R: Runtime> Plugin<R> for SwarmProxyApiPlugin<R> {
     fn name(&self) -> &'static str {
-        "k8s_proxy_api"
+        "swarm_proxy_api"
     }
     fn initialization_script(&self) -> Option<String> {
         None
---
+++ src/api/swarm_proxy.ts
@@ -0,0 +1,343 @@
+import { invoke } from '@tauri-apps/api/tauri';
+
+export type Label = {
+    key: string;
+    value: string;
+};
+
+export type ServiceInfo = {
+    service_id: string;
+    name: string;
+    label_list: Label[];
+    create_time: number;
+    update_time: number;
+    running_tasks: number;
+    desired_tasks: number;
+};
+
+export type TaskInfo = {
+    task_id: string;
+    name: string;
+    label_list: Label[];
+    service_id: string;
+    node_id: string;
+    status: string;
+    container_id: string;
+    image: string;
+    create_time: number;
+    update_time: number;
+};
+
+export type NameSpaceUserPerm = {
+    user_id: string;
+    update_scale: boolean;
+    update_image: boolean;
+    logs: boolean;
+    exec: boolean;
+};
+
+export type NameSpacePerm = {
+    name_space: string;
+    user_perm_list: NameSpaceUserPerm[];
+};
+
+export type ListNameSpaceRequest = {
+    token: string;
+};
+
+export type ListNameSpaceResponse = {
+    code: number;
+    err_msg: string;
+    name_space_list: string[];
+};
+
+
+export type ListServiceRequest = {
+    token: string;
+    name_space: string;
+};
+
+export type ListServiceResponse = {
+    code: number;
+    err_msg: string;
+    service_list: ServiceInfo[];
+};
+
+
+export type ListTaskRequest = {
+    token: string;
+    name_space: string;
+};
+
+export type ListTaskResponse = {
+    code: number;
+    err_msg: string;
+    task_list: TaskInfo[];
+};
+
+
+export type UpdateImageRequest = {
+    token: string;
+    name_space: string;
+    service_id: string;
+    image: string;
+};
+
+export type UpdateImageResponse = {
+    code: number;
+    err_msg: string;
+};
+
+
+export type UpdateScaleRequest = {
+    token: string;
+    name_space: string;
+    service_id: string;
+    scale: number;
+};
+
+export type UpdateScaleResponse = {
+    code: number;
+    err_msg: string;
+};
+
+
+export type GetNameSpacePermRequest = {
+    token: string;
+    name_space: string;
+};
+
+export type GetNameSpacePermResponse = {
+    code: number;
+    err_msg: string;
+    perm: NameSpacePerm;
+};
+
+
+export type SetNameSpacePermRequest = {
+    token: string;
+    name_space: string;
+    perm: NameSpacePerm;
+};
+
+export type SetNameSpacePermResponse = {
+    code: number;
+    err_msg: string;
+};
+
+
+export type OpenLogRequest = {
+
+    token: string;
+    namespace: string;
+    container_id: string;
+};
+
+export type OpenLogResponse = {
+    code: number;
+    err_msg: string;
+    log_id: string;
+};
+
+
+export type ReadLogRequest = {
+    log_id: string;
+};
+
+export type ReadLogResponse = {
+    code: number;
+    err_msg: string;
+    data: string;
+};
+
+
+export type OpenTermRequest = {
+    token: string;
+    namespace: string;
+    container_id: string;
+    shell_cmd: string;
+    term_width: number;
+    term_height: number;
+};
+
+export type OpenTermResponse = {
+    code: number;
+    err_msg: string;
+    term_id: string;
+};
+
+
+export type WriteTermRequest = {
+    term_id: string;
+    data: number[];
+};
+
+export type WriteTermResponse = {
+    code: number;
+    err_msg: string;
+};
+
+export type ReadTermRequest = {
+    term_id: string;
+};
+
+export type ReadTermResponse = {
+    code: number;
+    err_msg: string;
+    data: number[];
+};
+
+
+export type SetTermSizeRequest = {
+    term_id: string;
+    term_width: number;
+    term_height: number;
+};
+
+export type SetTermSizeResponse = {
+    code: number;
+    err_msg: string;
+};
+
+//列出所有命名空间
+export async function list_name_space(servAddr: string, request: ListNameSpaceRequest): Promise<ListNameSpaceResponse> {
+    const cmd = 'plugin:swarm_proxy_api|list_name_space';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<ListNameSpaceResponse>(cmd, {
+        servAddr,
+        request,
+    });
+}
+
+//列出所有service
+export async function list_service(servAddr: string, request: ListServiceRequest): Promise<ListServiceResponse> {
+    const cmd = 'plugin:swarm_proxy_api|list_service';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<ListServiceResponse>(cmd, {
+        servAddr,
+        request,
+    });
+}
+
+//列出所有task
+export async function list_task(servAddr: string, request: ListTaskRequest): Promise<ListTaskResponse> {
+    const cmd = 'plugin:swarm_proxy_api|list_task';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<ListTaskResponse>(cmd, {
+        servAddr,
+        request,
+    });
+}
+
+//更新镜像
+export async function update_image(servAddr: string, request: UpdateImageRequest): Promise<UpdateImageResponse> {
+    const cmd = 'plugin:swarm_proxy_api|update_image';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<UpdateImageResponse>(cmd, {
+        servAddr,
+        request,
+    });
+}
+
+//更新scale
+export async function update_scale(servAddr: string, request: UpdateScaleRequest): Promise<UpdateScaleResponse> {
+    const cmd = 'plugin:swarm_proxy_api|update_scale';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<UpdateScaleResponse>(cmd, {
+        servAddr,
+        request,
+    });
+}
+
+//获取命名空间权限
+export async function get_name_space_perm(servAddr: string, request: GetNameSpacePermRequest): Promise<GetNameSpacePermResponse> {
+    const cmd = 'plugin:swarm_proxy_api|get_name_space_perm';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<GetNameSpacePermResponse>(cmd, {
+        servAddr,
+        request,
+    });
+}
+
+//设置命名空间权限
+export async function set_name_space_perm(servAddr: string, request: SetNameSpacePermRequest): Promise<SetNameSpacePermResponse> {
+    const cmd = 'plugin:swarm_proxy_api|set_name_space_perm';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<SetNameSpacePermResponse>(cmd, {
+        servAddr,
+        request,
+    });
+}
+
+//开始查看日志
+export async function open_log(servAddr: string, request: OpenLogRequest): Promise<OpenLogResponse> {
+    const cmd = 'plugin:swarm_proxy_api|open_log';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<OpenLogResponse>(cmd, {
+        servAddr,
+        request,
+    });
+}
+
+//查看日志
+export async function read_log(servAddr: string, request: ReadLogRequest): Promise<ReadLogResponse> {
+    const cmd = 'plugin:swarm_proxy_api|read_log';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<ReadLogResponse>(cmd, {
+        servAddr,
+        request,
+    });
+}
+
+//打开终端
+export async function open_term(servAddr: string, request: OpenTermRequest): Promise<OpenTermResponse> {
+    const cmd = 'plugin:swarm_proxy_api|open_term';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<OpenTermResponse>(cmd, {
+        servAddr,
+        request,
+    });
+}
+
+//写入终端
+export async function write_term(servAddr: string, request: WriteTermRequest): Promise<WriteTermResponse> {
+    const cmd = 'plugin:swarm_proxy_api|write_term';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<WriteTermResponse>(cmd, {
+        servAddr,
+        request,
+    });
+}
+
+//读取终端
+export async function read_term(servAddr: string, request: ReadTermRequest): Promise<ReadTermResponse> {
+    const cmd = 'plugin:swarm_proxy_api|read_term';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<ReadTermResponse>(cmd, {
+        servAddr,
+        request,
+    });
+}
+
+//调整终端大小
+export async function set_term_size(servAddr: string, request: SetTermSizeRequest): Promise<SetTermSizeResponse> {
+    const cmd = 'plugin:swarm_proxy_api|set_term_size';
+    console.log(`%c${cmd}`, 'color:#0f0;', request);
+
+    return invoke<SetTermSizeResponse>(cmd, {
+        servAddr,
+        request,
+    });
+}
